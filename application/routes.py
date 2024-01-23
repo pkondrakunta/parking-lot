07c35e583d376bdf6537d6c08a62acd600ca4e3d
pkondrakunta <pragnyakondrakunta@gmail.com>
2024-01-22 21:45:56 -0500

    added some routes and templates

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,24 @@
 from application import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
-def home():
-    return "<h1>Welcome to Parkify, hello there!</h1>"
+def index():
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/create")
+def createTicket():
+    return render_template("createTicket.html")
+
+@app.route("/pay")
+def payForTicket():
+    return render_template("payTicket.html")
+
